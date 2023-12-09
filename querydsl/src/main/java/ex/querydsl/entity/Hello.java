50cbeebfce0b2c8e9fc41adda6dd39abf79aefca
junwvwv <wvwv0612@gmail.com>
2023-12-09 14:20:46 +0900

    Q클래스 검증

---
+++ querydsl/src/main/java/ex/querydsl/entity/Hello.java
@@ -0,0 +1,16 @@
+package ex.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
--- querydsl/src/test/java/ex/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/ex/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package ex.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import ex.querydsl.entity.Hello;
+import ex.querydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello findHello = queryFactory.selectFrom(qHello).fetchOne();
+		assertThat(findHello).isEqualTo(hello);
+		assertThat(findHello.getId()).isEqualTo(hello.getId());
 	}
 
 }
